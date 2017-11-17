646952c3977b675bf03c4171f07cc990823898fb
Tobias <Aupke@hotmail.com>
2017-11-17 15:29:49 +0100

    Notification Click is working

---
+++ app/src/main/java/com/example/aupke/geofencetestfriday/DistanceActivity.java
@@ -0,0 +1,19 @@
+package com.example.aupke.geofencetestfriday;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by Aupke on 17-11-2017.
+ */
+
+public class DistanceActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.distance);
+    }
+}
--- app/src/main/java/com/example/aupke/geofencetestfriday/IntentHandle.java
+++ app/src/main/java/com/example/aupke/geofencetestfriday/IntentHandle.java
@@ -4,6 +4,7 @@ import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
@@ -39,17 +40,27 @@ public class IntentHandle extends IntentService{
             notificationManager.createNotificationChannel(notificationChannel);
         }
 
+        Intent notificationClick = new Intent(this, DistanceActivity.class);
+        notificationClick.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        int time = (int) (System.currentTimeMillis() % 100000000);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(),time, notificationClick, 0);
+
+
         Notification notification = new NotificationCompat.Builder(this.getApplicationContext(), "default")
                 .setContentTitle(title)
                 .setContentText("Check out how to get to the Datalogi building!")
                 .setTicker("You're near the Hovedbanegaard. Do you want to see how to get to the CS building?")
                 .setSmallIcon(R.drawable.stat_sys_gps_on)
                 .setAutoCancel(true)
+                .setVibrate(new long[]{1500,0,1500,0})
+                .setContentIntent(pendingIntent)
                 .build();
 
         Log.d(MainActivity.TAG, "Notification created");
 
         NotificationManagerCompat manager = NotificationManagerCompat.from(getApplicationContext());
         manager.notify(1, notification);
+
+
     }
 }
