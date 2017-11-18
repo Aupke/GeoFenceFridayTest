5c56ce4d65b0a8e995250bae497408568d6e3bf5
Tobias <Aupke@hotmail.com>
2017-11-18 14:14:24 +0100

    Notifications work again

--- app/src/main/java/com/example/aupke/geofencetestfriday/StartActivity.java
+++ app/src/main/java/com/example/aupke/geofencetestfriday/StartActivity.java
@@ -20,6 +20,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
     }
 
     public void addGeofence(View view){
